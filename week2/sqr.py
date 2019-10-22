2e516cfdebd43edcf9a6366c239d75e9cf7c7cd9
alan <36449601+alanyuan97@users.noreply.github.com>
2019-10-22 16:08:54 +0100

    WK2: sqr fix

--- week2/sqr.py
+++ week2/sqr.py
@@ -12,8 +12,8 @@ BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be 
 M_RIGHT = BP.PORT_B
 M_LEFT = BP.PORT_C
 dps = 300
-TURN_VALUE = 1080 # TODO set this value 
-TURN_VALUE2 = 210 # TODO set this value
+TURN_VALUE = 1080 #  set this value 
+TURN_VALUE2 = 220 #  set this value
 try:
     try:
         BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B)) # reset encoder B
@@ -22,31 +22,25 @@ try:
         print(error)
 
     for index in range(0,4):
-    
-        BP.set_motor_dps(M_RIGHT,dps+3)
+        BP.set_motor_dps(M_RIGHT,dps)
         BP.set_motor_dps(M_LEFT,dps)
 
         while (BP.get_motor_encoder(M_LEFT)<TURN_VALUE):
             pass
-	print(f"Turning with encoder value: {BP.get_motor_encoder(M_RIGHT)}")
+        print(f"Turning with encoder value: {BP.get_motor_encoder(M_LEFT)} {BP.get_motor_encoder(M_RIGHT)}")
         BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B)) # reset encoder B
         BP.offset_motor_encoder(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C)) # reset encoder C
         # Turn function
-        BP.set_motor_dps(M_LEFT,-50)
-        BP.set_motor_dps(M_RIGHT,50)
-        
+        BP.set_motor_dps(M_LEFT,-103)
+        BP.set_motor_dps(M_RIGHT,100)
         while (BP.get_motor_encoder(M_RIGHT)<TURN_VALUE2):
             pass
-	print(f"Turn end")
+        print(f"Turn end")
+        print(f"Turning encoder :{BP.get_motor_encoder(M_LEFT)} {BP.get_motor_encoder(M_RIGHT)}")
         BP.offset_motor_encoder(BP.PORT_B, BP.get_motor_encoder(BP.PORT_B)) # reset encoder B
         BP.offset_motor_encoder(BP.PORT_C, BP.get_motor_encoder(BP.PORT_C)) # reset encoder C
 
     BP.reset_all() # Terminate
-    try:
-       # print(f"Motor B target encoder:{BP.get_motor_encoder(M_RIGHT)}")
-       # print(f"Motor C target encoder:{BP.get_motor_encoder(M_LEFT)}")
-    except IOError as error:
-        print(error)
 
     #time.sleep(0.02)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
 
